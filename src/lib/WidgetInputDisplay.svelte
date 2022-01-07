486552033a370d3291295bdb4d0e02dc32b6608f
Ron Au <5785323+ronvoluted@users.noreply.github.com>
2022-01-07 14:49:24 +1100

    feat(input): Add reset function - Fix small images not scaling

--- src/lib/WidgetInputDisplay.svelte
+++ src/lib/WidgetInputDisplay.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { loadedImage } from './stores';
+import { imageIsLoaded, loadedImage } from './stores';
 import WidgetInputDisplayNode from './WidgetInputDisplayNode.svelte';
 
 let inputImage: HTMLImageElement;
@@ -8,15 +8,24 @@ $: image = inputImage && {
   width: inputImage.clientWidth,
   height: inputImage.clientHeight,
 };
+
+const reset: EventListener = (event) => {
+  $imageIsLoaded = false;
+  $loadedImage = '';
+};
 </script>
 
 <div class="w-full rounded-lg h-fit">
+  <button
+    on:click={reset}
+    class="absolute px-2 py-1 text-xs border-2 rounded-lg border-transparent-blue right-4 top-4 bg-transparent-blue "
+    >Reset</button
+  >
   <h1 class="pb-1 mt-8 mb-4 text-xl text-left border-b-4 border-gray-300 border-dotted place-self-start">
     Object localisation
   </h1>
-  <!-- <div class="grid place-items-center"> -->
-  <div class="relative w-full h-full group">
-    <img bind:this={inputImage} src={$loadedImage} alt="" class="my-auto drop-shadow-lg" />
+  <div class="relative grid w-full h-full place-items-center group">
+    <img bind:this={inputImage} src={$loadedImage} alt="" class="w-full my-auto drop-shadow-lg" />
     <!-- <img bind:this={inputImage} src="/stuff.jpg" alt="" class="my-auto drop-shadow-lg" /> -->
     <!-- <img bind:this={inputImage} src="/building.jpg" alt="" class="my-auto drop-shadow-lg" /> -->
     <div class="absolute top-0 left-0 w-full h-full">
